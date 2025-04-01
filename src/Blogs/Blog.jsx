9a4f6cd3be86f5860b532cf60386d118f286eba1
digambar1411 <deshawaldigambar@gmail.com>
2025-04-01 17:30:56 +0530

    fix shadow

--- src/Blogs/Blog.jsx
+++ src/Blogs/Blog.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Blog({ blog }) {
   return (
-    <div className='p-4 hover:border rounded-xl shadow flex justify-between gap-4 items-start hover:box-border'>
+    <div className='p-4 hover:border rounded-xl shadow-lg flex justify-between gap-4 items-start hover:box-border'>
       <div>
         <h1 className='font-bold text-xl'>{blog.title}</h1>
         <p className='font-extralight my-1'>{blog.content}</p>
--- src/Projects/Project.jsx
+++ src/Projects/Project.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Project({ project }) {
   return (
-    <div className='p-4 hover:border rounded-xl shadow flex justify-between gap-4 items-start hover:box-border'>
+    <div className='p-4 hover:border rounded-xl shadow-lg flex justify-between gap-4 items-start hover:box-border'>
       <div>
         <h1 className='font-bold text-xl'>{project.title}</h1>
         <p className='font-extralight my-1'>{project.content}</p>
