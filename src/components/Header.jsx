0ca26cb6b67a7471b9104dc337ad586d64461f2c
digambar1411 <deshawaldigambar@gmail.com>
2025-04-03 20:37:23 +0530

    remove z-index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className='flex justify-between z-10 sticky h-16 p-4 top-0 box-border w-[100%] dark:bg-gray-950 dark:text-gray-100'> 
+    <div className='flex justify-between h-16 p-4 top-0 box-border w-[100%] dark:bg-gray-950 dark:text-gray-100'> 
       <a href='/'><h1 className='text-xl'> &lt; Digambar Deshawal /&gt;</h1></a>
       <nav>
         <a href="/blogs" className='dark:text-gray-100'>Blogs</a>
