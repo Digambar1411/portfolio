c61df187d4dae432dbdbedfdff7d9bb0e92ba8a3
digambar1411 <deshawaldigambar@gmail.com>
2025-04-03 23:48:24 +0530

    style: blog & project card responsiveness

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,12 +3,12 @@ function Blog({blog}) {
 
   return (
     <article key={blog.id} className='p-4 min-h-28 hover:transition rounded-xl shadow-md gap-4 items-start border border-transparent dark:text-gray-100 dark:border-gray-500! hover:border hover:border-black! hover:dark:border-gray-200!'>
-      <a href={`https://digambar.hashnode.dev/${blog.filename}`} className='flex justify-between' target="__blank">
-        <div className='w-[80%]'>
-          <h1 className='font-gray-700 text-xl'>{blog.title}</h1>
-          <p className='text-gray-500 dark:text-gray-400 font-light my-1'>{blog.content}</p>
+      <a href={`https://digambar.hashnode.dev/${blog.filename}`} target="__blank">
+        <div className='sm:flex justify-between'>
+          <h1 className='sm:w-[80%] font-gray-700 text-xl'>{blog.title}</h1>
+          <p className='text-gray-500 dark:text-gray-400 font-light text-sm'>{blog.publishDate}</p>
         </div>
-        <p className='text-gray-500 dark:text-gray-400 font-light text-sm'>{blog.publishDate}</p>
+        <p className='text-gray-500 dark:text-gray-400 font-light my-1 sm:w-[85%]'>{blog.content}</p>
       </a>
     </article>
   )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 function Project({ project }) {
   return (
-    <div className='min-h-32 p-4 hover:transition rounded-xl shadow-md flex justify-between gap-4 items-start border border-transparent dark:text-gray-100 dark:border-gray-500! hover:border hover:border-black'>
-      <div>
+    <div className='min-h-32 p-4 hover:transition rounded-xl shadow-md items-start border border-transparent dark:text-gray-100 dark:border-gray-500! hover:border hover:border-black'>
+      <div className='sm:flex justify-between'>
         <h1 className='font-gray-700 text-xl'>{project.title}</h1>
-        <p className='text-gray-500 dark:text-gray-400 font-light my-1'>{project.content}</p>
-        <a href={project.link} target='__blank' className='cursor-pointer underline underline-offset-4'>View</a>
+        <div className='text-gray-500 dark:text-gray-400 font-light text-sm max-sm:text-xs'>
+          <span>{project.startDate} - {project.endDate}</span>
+        </div>
       </div>
-      <div className='text-gray-500 dark:text-gray-400 font-light text-sm'>
-        <span>{project.startDate}</span> - <span>{project.endDate}</span>
+      <div>
+        <p className='text-gray-500 dark:text-gray-400 font-light my-1 sm:w-[85%]'>{project.content}</p>
+        <a href={project.link} target='__blank' className='cursor-pointer underline underline-offset-4'>View</a>
       </div>
     </div>
   )
