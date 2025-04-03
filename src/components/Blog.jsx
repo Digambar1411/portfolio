2ba8fef24a7fdaeea5321d8543f8ee074b1b8745
digambar1411 <deshawaldigambar@gmail.com>
2025-04-03 20:38:50 +0530

    style: add importat for border on card hover

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,7 +2,7 @@
 function Blog({blog}) {
 
   return (
-    <article key={blog.id} className='p-4 h-30 hover:transition rounded-xl shadow-md gap-4 items-start border border-transparent dark:text-gray-100 dark:border-gray-500! hover:border hover:border-black hover:dark:border-gray-200!'>
+    <article key={blog.id} className='p-4 h-30 hover:transition rounded-xl shadow-md gap-4 items-start border border-transparent dark:text-gray-100 dark:border-gray-500! hover:border hover:border-black! hover:dark:border-gray-200!'>
       <a href={`https://digambar.hashnode.dev/${blog.filename}`} className='flex justify-between' target="__blank">
         <div className='w-[80%]'>
           <h1 className='font-gray-700 text-xl'>{blog.title}</h1>
